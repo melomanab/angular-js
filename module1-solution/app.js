bf93c9c9a0d447dd441dead57528c694394abeac
bmoreno <beatriz.moreno@soprasteria.com>
2025-06-16 19:03:27 +0200

    Implement checkLunch() function

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,6 +7,28 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
 
+$scope.checkLunch = function(){
+    // Reinitialise variables
+    $scope.msg = ""
+    var items = ""
+
+    // Check empty input
+    if ($scope.input == null || $scope.input == ""){
+        $scope.msg = 'Please enter data first'
+        return
+    }
+
+    // Process input into items if not empty
+    items = ($scope.input).split(',')
+
+    if (items.length <= 3){
+        $scope.msg = 'Enjoy!'
+    }
+
+    if (items.length > 3){
+        $scope.msg =  'Too much!'
+    }
+};
 
 }
 
