15c7681e406d5cc2baaf7785abe32413ece64a92
bmoreno <beatriz.moreno@soprasteria.com>
2025-06-21 21:50:56 +0200

    Define ToBuyController with associated service

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,10 +1,43 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListCheckOffApp', []).
-controller('ToBuyController', ToBuyController).
-controller('AlreadyBoughtController',AlreadyBoughtController);
+angular.module('ShoppingListCheckOffApp', [])
+.controller('ToBuyController', ToBuyController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+// ToBuyController
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+    var toBuy = this;
 
+    // Initialize data on service
+    toBuy.initializeItemsToBuy =  function(){
+            ShoppingListCheckOffService.addItemToBuy('yaourt', '3');
+            ShoppingListCheckOffService.addItemToBuy('creme', '1');
+     }
+
+    toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+}
+
+// ShoppingListCheckOffService
+function ShoppingListCheckOffService(){
+    var service = this;
+
+    // List of items to buy
+    var itemsToBuy = [];
+
+    // Initialize
+    service.addItemToBuy = function(itemName, itemQuantity){
+        var item = {
+            name : itemName,
+            quantity: itemQuantity
+        };
+        itemsToBuy.push(item);
+    }
+
+    service.getItemsToBuy = function(){
+        return itemsToBuy;
+    }
+}
 
 })();
