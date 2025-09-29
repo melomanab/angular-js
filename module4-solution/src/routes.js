447f2b1aaf29097892e2b05820f91f306b8a8dce
Beatriz MORENO <moreno.ortega.beatriz@gmail.com>
2025-09-29 14:16:37 +0200

    Define categories state

--- module4-solution/src/routes.js
+++ module4-solution/src/routes.js
@@ -19,6 +19,20 @@
                 templateUrl: 'src/menu/templates/home.template.html'
                 }
             )
+        // Categories page
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'src/menu/templates/main-categories.template.html',
+                controller: 'MainCategoriesController as categoriesList',
+                // The resolve will use the MenuDataService to retrieve categories
+                // and inject them into the controller.
+                resolve: {
+                    items: ['MenuDataService', function(MenuDataService){
+                        console.log("--> Invoke MenuDataService.getAllCategories()");
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
 
     }
 
