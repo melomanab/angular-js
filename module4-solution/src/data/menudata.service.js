9589e8517343a857ed7b2424f33cb4f7ec025dab
Beatriz MORENO <moreno.ortega.beatriz@gmail.com>
2025-09-29 14:19:04 +0200

    Implement service retrieving categories from the server

--- module4-solution/src/data/menudata.service.js
+++ module4-solution/src/data/menudata.service.js
@@ -6,24 +6,27 @@ angular.module('Data')
     .service('MenuDataService', MenuDataService);
 
 // Implement service function relying on http ajax client
-MenuDataService.$inject = ['$http'];
-function MenuDataService($http) {
+MenuDataService.$inject = ['$q','$http'];
+function MenuDataService($q, $http) {
     var service = this;
 
 
     service.getAllCategories = function () {
         console.log("--> getAllCategories");
-        return $http(
-            {
-                method: 'GET',
-                url: 'https://coursera-jhu-default-rtdb.firebaseio.com/categories.json',
-            }
-        ).then(
-            function(response) {
-                // Returns a promise
-                return response;
-            }
-        );
+
+        var deferred = $q.defer();
+
+        $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/categories.json')
+            .then(function (response) {
+                setTimeout(function () {
+                    deferred.resolve(response.data);
+                }, 100);
+            })
+            .catch(function (error) {
+                deferred.reject(error);
+            });
+        return deferred.promise;
+
     } // end of getAllCategories
 
     service.getItemsForCategory = function (categoryShortName) {
